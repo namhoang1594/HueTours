287a5cbc645d242341b09b6ca9b14d4ea27b69f7
Hoang Nhat Nam <nam.hoangnhat@phuxuan.edu.vn>
2021-07-22 23:31:13 +0700

    update 22/7

---
+++ Huetours/js/style.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+	$(function () {
+		$('.title_faq').on('click', function (event) {
+			event.preventDefault();
+			var $this = $(this);
+			if ($this.next().css('display') != 'block') {
+				$('.content').slideUp(100);
+				$('.icon_faq').removeClass('active_faq');
+				$('.title_faq').removeClass('active_title');
+
+				$this.addClass('active_title');
+				$this.next().slideDown(100);
+				$this.children('.icon_faq').addClass('active_faq');
+			}
+			else {
+				$this.removeClass('active_title');
+				$this.next().slideUp(100);
+				$this.children('.icon_faq').removeClass('active_faq');
+			}
+		});
+	});
+}
