a52e1bfe72fcbd90c529df8201262d22435c6ed5
Hoàng Nam <nam.hn@3si.vn>
2021-08-24 15:20:17 +0700

    update css home page

--- Huetours/App_Data/Models/Home.generated.cs
+++ Huetours/App_Data/Models/Home.generated.cs
@@ -52,6 +52,13 @@ namespace Umbraco.Web.PublishedModels
 		public virtual global::System.Collections.Generic.IEnumerable<global::Umbraco.Core.Models.MediaWithCrops> AdvertisementImage => this.Value<global::System.Collections.Generic.IEnumerable<global::Umbraco.Core.Models.MediaWithCrops>>("advertisementImage");
 
 		///<summary>
+		/// Advertisement Title
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "8.15.0")]
+		[ImplementPropertyType("advertisementTitle")]
+		public virtual string AdvertisementTitle => this.Value<string>("advertisementTitle");
+
+		///<summary>
 		/// Banner Image
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "8.15.0")]
